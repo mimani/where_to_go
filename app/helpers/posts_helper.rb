1ae5f2b0fe9ee875d089fdec4be8de14d9ec0182
saurabh.mimani <saurabh.mimani@flipkart.com>
2014-12-06 14:31:34 +0530

    first commit

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,16 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module SampleBlog
+  class App
+    module PostsHelper
+      # feed_tag(:rss, :title => "RSS", :href => url(...))
+      # feed_tag(:atom, :title => "ATOM", :href => url(...))
+      def feed_tag(mime, options={})
+        full_mime = (mime == :atom) ? 'application/atom+xml' : 'application/rss+xml'
+        content_tag(:link, options.reverse_merge(:rel => 'alternate', :type => full_mime))
+      end
+    end
+
+    helpers PostsHelper
+  end
+end
